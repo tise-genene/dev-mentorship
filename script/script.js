ecce11d4065b4c581f45a63cb6599efb1918b029
Degife Tise <degifetise09@gmail.com>
2025-11-29 10:24:39 +0300

    portfolio (#6)

--- script/script.js
+++ script/script.js
@@ -17,33 +17,37 @@ toggle.addEventListener("click", () => {
 
 //typing text about me//
 
-const texts = ["Frontend Developer", "UI/UX Designer", "Backend Developer", "Computer Technician"];
+const texts = [
+  "Frontend Developer",
+  "UI/UX Designer",
+  "Backend Developer",
+  "Computer Technician",
+];
 
 const typingEl = document.getElementById("typing-text");
-let index = 0, charIndex = 0;
+let index = 0,
+  charIndex = 0;
 
 function typeEffect() {
-
-if(charIndex < texts[index].length) {
-
-  typingEl.innerHTML += texts[index].charAt(charIndex);
-  charIndex++;
-  setTimeout(typeEffect, 100);
-} else {
-  setTimeout(eraseEffect, 1500);
-}
+  if (charIndex < texts[index].length) {
+    typingEl.innerHTML += texts[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(eraseEffect, 1500);
+  }
 }
 //function to erase texts//
 
 function eraseEffect() {
-if(charIndex > 0) {
-  typingEl.innerHTML = texts[index].substring(0, charIndex - 1);
-  charIndex--;
-  setTimeout(eraseEffect, 50);
-} else {
-  index = (index + 1) % texts.length;
-  setTimeout(typeEffect, 500);
-}
+  if (charIndex > 0) {
+    typingEl.innerHTML = texts[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 50);
+  } else {
+    index = (index + 1) % texts.length;
+    setTimeout(typeEffect, 500);
+  }
 }
 typeEffect();
 eraseEffect();
@@ -51,20 +55,17 @@ eraseEffect();
 
 const navbar = document.getElementById("navbar");
 const bar = document.getElementById("bar");
-  const barHide = document.querySelector("#barTimesBtn");
+const barHide = document.querySelector("#barTimesBtn");
 bar.addEventListener("click", () => {
   navbar.classList.toggle("showNavbar");
-if(navbar.classList.contains("showNavbar")) {
-barHide.classList.toggle("fa-times");
- barHide.classList.remove("fa-bars");
-} else {
-barHide.classList.remove("fa-times");
-barHide.classList.add("fa-bars");
-}
-
+  if (navbar.classList.contains("showNavbar")) {
+    barHide.classList.toggle("fa-times");
+    barHide.classList.remove("fa-bars");
+  } else {
+    barHide.classList.remove("fa-times");
+    barHide.classList.add("fa-bars");
+  }
 });
-
-
 
 //date  todays//
 
